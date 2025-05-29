f7ec4fb426cfa8e34bc15207dd44279bba686a6a
Radmir Taskynbayev <87788000800@mail.ru>
2025-05-28 21:59:36 -0400

    chore: add Netlify adapter for SSR

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,16 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
+import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
 export default defineConfig({
+ // 1. Tell Astro to output a server bundle for Netlify Functions
+ output: 'server',
+
+ // 2. Use the Netlify adapter
+ adapter: netlify(),
+
   integrations: [
     react(),
     tailwind(),

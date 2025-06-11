f822cffce3bdd814ae391a5f7df2d447f0750fc8
Radmir Taskynbayev <87788000800@mail.ru>
2025-06-11 19:18:37 -0400

    netlify deployment prep

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,16 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
+import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
-  redirects: {
-    '/': '/home'
-  }
+  output: 'server',
+  adapter: netlify({
+    edge: true,
+    include: ['**/*'],
+    exclude: ['**/*.{png,jpg,jpeg,gif,svg,webp,ico,woff,woff2,eot,ttf,otf,webmanifest,xml,json,txt,csv,doc,docx,xls,xlsx,ppt,pptx,zip,tar,gz,bz2,rar,7z,pdf}'],
+    functionsDir: './functions'
+  })
 });

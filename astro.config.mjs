bb34347bb588139a1834b1866df8c5151b231569
Radmir Taskynbayev <87788000800@mail.ru>
2025-06-11 19:43:16 -0400

    netlify deployment fix 3

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,10 +7,8 @@ import netlify from '@astrojs/netlify';
 export default defineConfig({
   integrations: [react()],
   output: 'server',
-  adapter: netlify({
-    edge: true,
-    include: ['**/*'],
-    exclude: ['**/*.{png,jpg,jpeg,gif,svg,webp,ico,woff,woff2,eot,ttf,otf,webmanifest,xml,json,txt,csv,doc,docx,xls,xlsx,ppt,pptx,zip,tar,gz,bz2,rar,7z,pdf}'],
-    functionsDir: './functions'
-  })
+  adapter: netlify(),
+  redirects: {
+    '/': '/home'
+  }
 });
